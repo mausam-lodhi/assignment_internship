91757b31dc0d307f6adf4ab8b8d8396dc4b374cc
mausam-lodhi <harprasadlodhi1984@gmail.com>
2025-03-24 16:02:11 +0530

    first commit

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import Profile from "../models/profile.js";
 
 dotenv.config();
